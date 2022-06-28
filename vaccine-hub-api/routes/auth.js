23d2396b97912a132dc2e807215331663ee9672f
jennymylee <jennylee038@gmail.com>
2022-06-28 10:34:31 -0700

    set up auth routes

---
+++ vaccine-hub-api/routes/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/login", async (req, res, next) => {
+  try {
+    // take users email and password and attempt to authenticate them
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+  try {
+    // take users email, password, rsvp status, and the number of guests
+    // and create a new user in our database
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
---
+++ vaccine-hub-api/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const { BadRequestError, NotFoundError } = require("./utils/errors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+app.use((error, req, res, next) => {
+  let status = error.status || 500;
+  let message = error.message || "Something went wrong in the application";
+  return res.status(status).json({ error: { status, message } });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running https://localhost:${PORT}`);
+});
